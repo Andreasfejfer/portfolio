689ee97583ecf6a96bb93a94befafcf4d5c82c97
Andreasfejfer <98988659+Andreasfejfer@users.noreply.github.com>
2025-12-16 12:43:17 +0100

    fix

--- js/page-home.js
+++ js/page-home.js
@@ -1,12 +1,7 @@
-import { initScramble } from "./scramble.js";
 import { initPreloader } from "./preloader.js";
+import { initMainScrambleGated } from "./scramble.js";
 
-export async function initHomePage() {
-  await initPreloader({
-    // justér hvis din preloader-tekst skal have mere/mindre tid
-    preloaderTextWaitMs: 1800,
-  });
-
-  // Alt andet starter først efter preloader er færdig
-  initScramble();
+export function initHomePage() {
+  initPreloader();
+  initMainScrambleGated();
 }
