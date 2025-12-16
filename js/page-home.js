4126c9734106df14510c631834265ee65880afda
Andreasfejfer <98988659+Andreasfejfer@users.noreply.github.com>
2025-12-16 12:02:56 +0100

    Test run
    test run for webflow

--- js/page-home.js
+++ js/page-home.js
@@ -1,5 +1,5 @@
-import { initScramble } from "./scramble.js";
-import { initPreloader } from "./preloader.js";
+import { initScramble } from "./scramble.js?v=1";
+import { initPreloader } from "./preloader.js?v=1";
 
 export function initHomePage() {
   initPreloader();
