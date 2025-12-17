37e8eed9da5eefc0b6e3578ad73dad065e63ae39
Andreasfejfer <98988659+Andreasfejfer@users.noreply.github.com>
2025-12-17 18:33:04 +0100

    gsap repetition

--- js/page-home.js
+++ js/page-home.js
@@ -1,7 +1,9 @@
 import { initPreloader } from "./preloader.js";
 import { initScramble } from "./scramble.js";
+import { initRepetitionEffectGSAP } from "./repitetion.js";
 
 export function initHomePage() {
   initPreloader();
   initScramble();
+  initRepetitionEffectGSAP();
 }
