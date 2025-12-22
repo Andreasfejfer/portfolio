f8cb7f4bd1e3fcab5635716f215265c813df1998
Andreasfejfer <98988659+Andreasfejfer@users.noreply.github.com>
2025-12-22 11:18:19 +0100

    fix

--- js/page-home.js
+++ js/page-home.js
@@ -2,11 +2,15 @@
 
 export function initHomePage() {
   initPreloader();
-  // Only run scramble after preloader is done (and session key is set)
+  // Always run preloader scramble (handled inside preloader.js)
+  // Only run main scramble after preloader is done (and session key is set)
+  if (window.__PRELOADER_DONE === true) {
+    // If preloader already done (internal navigation), do not run scramble
+    return;
+  }
   window.addEventListener("preloader:done", () => {
     setTimeout(() => {
       initScramble();
     }, 0);
   }, { once: true });
-  // If preloader already done (e.g. on internal navigation), do not run scramble
 }
