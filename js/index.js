756e3cc3b607e88945c1a15771a7e64b0282bb37
Andreasfejfer <98988659+Andreasfejfer@users.noreply.github.com>
2025-12-22 10:29:05 +0100

    revert

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 import { initMarquee } from './marquee.js';
-import { initPreloader } from './preloader.js';
 
-// Initialize marquee and preloader only if body has 'page-index' class
+// Initialize marquee only if body has 'page-index' class
 document.addEventListener('DOMContentLoaded', () => {
 	if (document.body.classList.contains('page-index')) {
-		initPreloader();
 		document.querySelectorAll('.s-marquee').forEach(el => {
 			initMarquee({ selector: el, speed: 20 });
 		});
