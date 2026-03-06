973a410ac9962472a18f07ee85f32362bd79dae1
Andreasfejfer <98988659+Andreasfejfer@users.noreply.github.com>
2026-03-06 17:56:40 +0100

    fix

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ import { initMarquee } from './marquee.js';
 // Initialize marquee only if body has 'page-index' class
 document.addEventListener('DOMContentLoaded', () => {
 	if (document.body.classList.contains('page-index')) {
-		document.querySelectorAll('.s-marquee').forEach(el => {
-			initMarquee({ selector: el, speed: 40 });
-		});
+			document.querySelectorAll('.s-marquee, .marquee').forEach(el => {
+				initMarquee({ selector: el, speed: 40 });
+			});
 	}
 });
